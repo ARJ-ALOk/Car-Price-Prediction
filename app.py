7e681c2f61f442e9060e086967cadb0b09e814d6
Alokraj <alokrajpandey1111@gmail.com>
2026-07-21 22:51:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,51 +1,117 @@
-import pandas as pd 
-import numpy as np 
-import pickle as pk 
+from pathlib import Path
+import pickle as pk
+
+import pandas as pd
 import streamlit as st
+from sklearn.preprocessing import LabelEncoder
 
-model = pk.load(open('model.pkl','rb'))
+base_dir = Path(__file__).resolve().parent
+model = pk.load(open(base_dir / "model.pkl", "rb"))
 
-st.header('Car Price Prediction ML Model')
+st.set_page_config(
+    page_title="Car Price Prediction",
+    page_icon="🚗",
+    layout="wide",
+    initial_sidebar_state="expanded",
+)
 
-cars_data = pd.read_csv('Cardetails.csv')
-
-def get_brand_name(car_name):
-    car_name = car_name.split(' ')[0]
-    return car_name.strip()
-cars_data['name'] = cars_data['name'].apply(get_brand_name)
-
-name = st.selectbox('Select Car Brand', cars_data['name'].unique())
-year = st.slider('Car Manufactured Year', 1994,2024)
-km_driven = st.slider('No of kms Driven', 11,200000)
-fuel = st.selectbox('Fuel type', cars_data['fuel'].unique())
-seller_type = st.selectbox('Seller  type', cars_data['seller_type'].unique())
-transmission = st.selectbox('Transmission type', cars_data['transmission'].unique())
-owner = st.selectbox('Seller  type', cars_data['owner'].unique())
-mileage = st.slider('Car Mileage', 10,40)
-engine = st.slider('Engine CC', 700,5000)
-max_power = st.slider('Max Power', 0,200)
-seats = st.slider('No of Seats', 5,10)
+car_data = pd.read_csv(base_dir / "Cardetails.csv")
 
 
-if st.button("Predict"):
-    input_data_model = pd.DataFrame(
-    [[name,year,km_driven,fuel,seller_type,transmission,owner,mileage,engine,max_power,seats]],
-    columns=['name','year','km_driven','fuel','seller_type','transmission','owner','mileage','engine','max_power','seats'])
-    
-    input_data_model['owner'].replace(['First Owner', 'Second Owner', 'Third Owner',
-       'Fourth & Above Owner', 'Test Drive Car'],
-                           [1,2,3,4,5], inplace=True)
-    input_data_model['fuel'].replace(['Diesel', 'Petrol', 'LPG', 'CNG'],[1,2,3,4], inplace=True)
-    input_data_model['seller_type'].replace(['Individual', 'Dealer', 'Trustmark Dealer'],[1,2,3], inplace=True)
-    input_data_model['transmission'].replace(['Manual', 'Automatic'],[1,2], inplace=True)
-    input_data_model['name'].replace(['Maruti', 'Skoda', 'Honda', 'Hyundai', 'Toyota', 'Ford', 'Renault',
-       'Mahindra', 'Tata', 'Chevrolet', 'Datsun', 'Jeep', 'Mercedes-Benz',
-       'Mitsubishi', 'Audi', 'Volkswagen', 'BMW', 'Nissan', 'Lexus',
-       'Jaguar', 'Land', 'MG', 'Volvo', 'Daewoo', 'Kia', 'Fiat', 'Force',
-       'Ambassador', 'Ashok', 'Isuzu', 'Opel'],
-                          [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31]
-                          ,inplace=True)
+def model_name(carName):
+    carName = carName.split()[0]
+    return carName
 
-    car_price = model.predict(input_data_model)
 
-    st.markdown('Car Price is going to be '+ str(car_price[0])
+car_data["name"] = car_data["name"].apply(model_name)
+
+encoders = {
+    "fuel": LabelEncoder().fit(car_data["fuel"]),
+    "seller_type": LabelEncoder().fit(car_data["seller_type"]),
+    "name": LabelEncoder().fit(car_data["name"]),
+    "owner": LabelEncoder().fit(car_data["owner"]),
+    "transmission": LabelEncoder().fit(car_data["transmission"]),
+}
+
+car_name = st.selectbox("Select Car Brand", sorted(car_data["name"].unique()))
+
+mf_year = st.slider("Car Manufactured year", 1994, 2026, 2010)
+km = st.slider("No. of KMs Driven", 10, 1000000, 500000)
+fuel = st.selectbox("Fuel type", ["Diesel", "Petrol", "CNG", "LPG"])
+seller = st.selectbox("Seller type", ["Individual", "Dealer", "TrustMark Dealer"])
+
+trans = st.selectbox("Transmission Type", ["Automatic", "Manual"])
+
+Owner = st.selectbox("Owner Type", ["First Owner", "Second Owner", "Third Owner", "Others"])
+mil = st.slider("Car mileage", 10, 50, 25)
+eng = st.slider("Engine CC", 700, 4000, 2000)
+max_p = st.slider("Max Power", 0, 400, 200)
+seats = st.slider("No. of seats", 2, 10, 4)
+
+pred = st.button("Predict")
+
+
+if pred:
+    @st.dialog("Car details")
+    def show_details():
+        st.header("Car Details:")
+        c1, c2, c3 = st.columns([1.5, 1, 1.5])
+        with c1:
+            with st.container(border=True):
+                st.caption("🚗 Car Model")
+                st.subheader(car_name)
+            with st.container(border=True):  
+                st.caption("Manufactured Year ")
+                st.subheader(mf_year)
+            with st.container(border=True):    
+                st.caption("Car mileage")
+                st.subheader(mil)
+            with st.container(border=True):
+                st.caption("Total Seats")
+                st.subheader(seats)
+        with c2:
+            with st.container(border=True):
+                st.caption("Fuel type")
+                st.subheader(fuel)
+            with st.container(border=True):
+                st.caption("No. of KMs Driven")
+                st.subheader(km)
+            with st.container(border=True):
+                st.caption("Engine")
+                st.subheader(eng)
+        with c3:
+            with st.container(border=True):
+                st.caption("Seller type")
+                st.subheader(seller)
+            with st.container(border=True):
+                st.caption("Owner type")
+                st.subheader(Owner)
+            with st.container(border=True):
+                st.caption("Max Power")
+                st.subheader(max_p)
+
+        input_car_data = pd.DataFrame(
+            {
+                "name": [car_name],
+                "year": [mf_year],
+                "km_driven": [km],
+                "fuel": [fuel],
+                "seller_type": [seller],
+                "transmission": [trans],
+                "owner": [Owner],
+                "mileage": [mil],
+                "engine": [eng],
+                "max_power": [max_p],
+                "seats": [seats],
+            }
+        )
+
+        for column, encoder in encoders.items():
+            input_car_data[column] = encoder.transform(input_car_data[column].astype(str))
+
+        car_price = model.predict(input_car_data)
+        predicted_price = car_price[0]
+
+        st.success(f"💰 Predicted Car Price: ₹ {predicted_price:,.2f}")
+
+    show_details()
